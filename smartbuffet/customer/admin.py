afbe453a5039b3c6461cde0ff5c34727db307c14
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-01-08 12:44:40 +0900

    create virtual_env

--- smartbuffet/customer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import tablestableinstore , tabeldailydate , meat , order
-# Register your models here.
-admin.site.register(tablestableinstore)
-admin.site.register(tabeldailydate)
-admin.site.register(meat)
-admin.site.register(order)
