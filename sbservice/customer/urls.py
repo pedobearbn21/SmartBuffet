b60cc8aa3d759b012af6dccc06c940f8f8e0ef8e
PedobearEarn <39798687+pedobearbn21@users.noreply.github.com>
2021-01-09 15:34:06 +0900

    set APi

---
+++ sbservice/customer/urls.py
@@ -0,0 +1,28 @@
+"""sbservice URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from customer.views import OrderView, OrderIDView, MeatView, MeatIDView,Search,GraphTotalCustomer
+
+
+urlpatterns = [
+    path('orderlist', OrderView.as_view()),
+    path('order/<int:id>', OrderIDView.as_view()),
+    path('meatlist', MeatView.as_view()),
+    path('meat/<int:id>', MeatIDView.as_view()),
+    path('meat/search/<int:type>', Search.as_view()),
+    #Graph Total Customer Today
+    path('totalcustomer', GraphTotalCustomer.as_view())
+]
---
+++ sbservice/customer/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from customer.models import TablestableInStore , Tabledailydate , Meats , Orders
+from rest_framework import generics
+from customer.serializers import OrderSerializers, MeatSerializers, TableDailySerializers
+from django.utils import timezone
+
+# Create your views here.
+
+class OrderView(generics.ListCreateAPIView):
+    queryset = Orders.objects.all()
+    serializer_class = OrderSerializers
+
+class OrderIDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Orders.objects.all()
+    lookup_field = 'id'
+    serializer_class = OrderSerializers
+
+
+class MeatView(generics.ListCreateAPIView):
+    queryset = Meats.objects.all()
+    serializer_class = MeatSerializers
+
+class MeatIDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Meats.objects.all()
+    lookup_field = 'id'
+    serializer_class = MeatSerializers
+
+class Search(generics.ListAPIView):
+    serializer_class = MeatSerializers
+    lookup_url_kwarg = 'type'
+
+    def get_queryset(self):
+        type_id = self.kwargs.get(self.lookup_url_kwarg)
+        meatlist = Meats.objects.filter(type=type_id)
+        return meatlist
+
+class GraphTotalCustomer(generics.ListAPIView):
+    serializer_class = TableDailySerializers
+    
+    def get_queryset(self):
+        total = Tabledailydate.objects.filter(table_open_time__date = timezone.now())
+        return total
